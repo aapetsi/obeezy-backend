bd33c3c3f3295824110b0e62ddcdb2dbceed8b55
aapetsi <apetsiampiah@gmail.com>
2020-04-23 19:58:50 +0000

    add cors middleware

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }))
+
+// Cors middleware
+app.use(cors)
 
 module.exports = app
