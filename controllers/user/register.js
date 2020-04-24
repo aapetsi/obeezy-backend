baf89e7c3144fa4c6629e0d3713c7ff92d42b442
aapetsi <apetsiampiah@gmail.com>
2020-04-24 07:31:12 +0000

    add register route

---
+++ controllers/user/register.js
@@ -0,0 +1,8 @@
+const bcrypt = require('bcryptjs')
+const User = require('../../models/User')
+
+const register = async (req, res) => {
+  res.send('register')
+}
+
+module.exports = register
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,9 +2,14 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/user')
 
-// @route POST api/user/login
+// @route POST api/users/login
 // @desc Login user
 // @access Public
 router.post('/login', userController.login)
+
+// @route POST api/users/register
+// @desc Register a new user
+// @access Public
+router.post('/register', userController.register)
 
 module.exports = router
