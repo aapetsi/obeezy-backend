4947651f80e85474671eb989e12a2a87f6210679
aapetsi <apetsiampiah@gmail.com>
2020-04-24 07:52:04 +0000

    move port value to env

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require('./server')
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 server.listen(port, () => console.log(`Server is running on port ${port}`))
